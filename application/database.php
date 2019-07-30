6bfe08532de6c9ac5aadd0ddbae573d92dd6d52e
wangdewen <17076266402@163.com>
2019-07-30 18:03:02 +0800

    框架上传

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '111.230.11.122',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'www.51dewen.wang',
+    'database'        => 'bearadmin',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'wang6036',
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
